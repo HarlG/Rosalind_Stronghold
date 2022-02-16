aca8faada5ff5e0149f5c0d302ad6c0b5e838a97
HarlG <harlan.gillespie1@gmail.com>
2022-02-16 18:19:42 +0000

    Dictionary used to count each nucleotide. Printed in accordance with given format

--- counting_DNA_nucleotides.py
+++ counting_DNA_nucleotides.py
@@ -1,9 +1,34 @@
-string = 'ATGCTTCAGAAAGGTCTTACG'
+"""Problem
+
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+Given: A DNA string s
+
+of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s
+
+
+Sample Dataset
+
+AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+
+Sample Output
+
+20 12 17 21 """
+
+
+string = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
 
 def count_nuc(string):
     count_dic = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
     for s in string:
-        print(s)
+        count_dic[s] +=1
+    return count_dic
 
 
-count_nuc(string)
+result=count_nuc(string)
+
+print(' '.join([str(val) for key, val in result.items()]))
